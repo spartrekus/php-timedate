c0e7554799822bde6113551162b3fb5311441bfc
spartrekus <spartrekus@gmail.com>
2018-06-03 11:53:59 +0200

    Add files via upload

---
+++ timedate.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+
+echo "The time is " . date("h:i:sa");
+
+?>
+
+</body>
+</html>
